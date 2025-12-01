39a573b8135b5c2e128fdfced5f5ac9f04f2fecf
Jackjon Butcher <jackjon@justjackjon.dev>
2025-12-01 16:45:48 +0000

    :bug:  (roles): Add linux fixes

--- install.sh
+++ install.sh
@@ -52,9 +52,20 @@ if [ "$os" == "Linux" ]; then
   sudo apt-get update
 
   info "Installing dependencies (git, python3, ansible)..."
-  sudo apt-get install -y git python3 python3-pip python3-venv pipx gh
+  sudo apt-get install -y git python3 python3-pip python3-venv pipx
   pipx ensurepath
   pipx install ansible ansible-core
+
+  info "Installing latest gh cli..."
+  (type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
 
   info "Installing Ansible collections..."
   "$HOME/.local/bin/ansible-galaxy" collection install community.general ansible.windows community.crypto community.windows
